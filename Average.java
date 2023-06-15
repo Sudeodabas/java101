5fbfe84cddaf89ae5cb5ae012b99c6604a34e745
Sude Odabaş <102865553+Sudeodabas@users.noreply.github.com>
2023-06-15 12:16:48 +0300

    Update and rename Avarage.java to Average.java

--- Average.java
+++ Average.java
@@ -1,11 +1,11 @@
 public class Avarage {
     public static void main(String[] args) {
         int[] list={1,2,3,4,5};
-        double avarage=0.0;
+        double average=0.0;
         for(int i=0;i<list.length;i++){
-            avarage+=list[i];
+            average+=list[i];
         }
-        avarage/=list.length;
-        System.out.println(avarage);
+        average/=list.length;
+        System.out.println(average);
     }
 }
